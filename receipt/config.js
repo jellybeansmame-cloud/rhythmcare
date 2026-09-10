61af42dadfd346f05ffa6b9e82a170af92a2d262
Yachiyo Hasegawa <jellybeansmame@gmail.com>
2026-09-11 06:58:35 +0900

    Remove receipt PWA shell (belongs in ebay-post-app repo)

--- receipt/config.js
+++
@@ -1,5 +0,0 @@
-// 郵便レシート GAS（ebay-sales EBAY_CONFIG.WEB_APP_Post_URL と同じ）
-window.RECEIPT_GAS_URL =
-  'https://script.google.com/macros/s/AKfycbzcsDQh_AXyEKS7Kzj_YG_y_5mZQUV2L41MSe0oP7CLDDPPAKi833bPpTYSS5VlrWy5/exec';
-
-window.RECEIPT_SS_ID = '';
